e09b7811f21334c6bd38f11fe3f377fd09c625d8
Jason <jasylwong@gmail.com>
2020-05-08 12:05:00 +0100

    complete ex3.1

--- index.js
+++ index.js
@@ -1,9 +1,36 @@
 const express = require('express')
 const app = express()
 
+let notes = [
+  {
+    "id": 1,
+    "name": "Arto Hellas",
+    "number": "040-123456"
+  },
+  {
+    "id": 2,
+    "name": "Ada Lovelace",
+    "number": "39-44-5323523"
+  },
+  {
+    "id": 3,
+    "name": "Dan Abramov",
+    "number": "12-43-234345"
+  },
+  {
+    "id": 4,
+    "name": "Mary Poppendieck",
+    "number": "39-23-6423122"
+  }
+]
+
 app.get('/', (req, res) => 
-  res.send('hello world!! This is my phonebook app')
+  res.send('Hello world!! This is my phonebook app, created using Node.js')
 )
+
+app.get('/api/persons', (req, res) => {
+  res.json(notes)
+})
 
 const PORT = 3001
 app.listen(PORT, () => {
