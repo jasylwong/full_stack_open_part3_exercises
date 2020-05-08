f94834a1bca519b626ec134bf489baea6f2d37a0
Jason <jasylwong@gmail.com>
2020-05-08 12:26:06 +0100

    complete ex3.2

--- index.js
+++ index.js
@@ -32,6 +32,14 @@ app.get('/api/persons', (req, res) => {
   res.json(notes)
 })
 
+app.get('/info', (req, res) => {
+  console.log(Date.now())
+  res.send(`
+    <p>Phonebook has info for ${notes.length} people</p>
+    <p>${new Date}</p>
+  `)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
