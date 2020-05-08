d824935cc05336d145f716071dfd832e8ab16cc2
Jason <jasylwong@gmail.com>
2020-05-08 18:21:00 +0100

    add requestLogger

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
 
 app.use(express.json())
+
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method);
+  console.log('Path:', request.path);
+  console.log('Body:', request.body);
+  console.log('---');
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(requestLogger)
 
 let persons = [
   {
