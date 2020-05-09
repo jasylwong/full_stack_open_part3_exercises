41fc07b9a5bc4731372bc394c95a69d450edffcd
Jason <jasylwong@gmail.com>
2020-05-09 14:30:19 +0100

    update build

---
+++ build/precache-manifest.1f8bd38903a2d1dd7fdefa7db35d88a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8313d1fea539a44b0fabf90f245a99a8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9ed18200ee861c53de67",
+    "url": "/static/css/main.a32a41f6.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ed18200ee861c53de67",
+    "url": "/static/js/main.bc79538d.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);
