d516db97ca6857f0e7b45163ddaf61b9f82bb79b
Jason <jasylwong@gmail.com>
2020-05-10 13:24:32 +0100

    conditional for parameters

--- mongo.js
+++ mongo.js
@@ -6,24 +6,12 @@ if (process.argv.length < 3) {
 }
 const password = process.argv[2]
 
-if (process.argv.length < 4) {
-  console.log('give name as argument')
+if (process.argv.length === 4) {
+  console.log('give both name and number as arguments')
   process.exit(1)
 }
 const name = process.argv[3]
-
-if (process.argv.length < 5) {
-  console.log('give number as argument')
-  process.exit(1)
-}
 const number = process.argv[4]
-
-console.log(password);
-console.log(name);
-console.log(number);
-
-process.exit(1)
-
 
 const url = `mongodb+srv://jasylwong:${password}@cluster0-7yxwh.mongodb.net/phonebook?retryWrites=true&w=majority`
 
@@ -36,20 +24,21 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-// const person = new Person({
-//   name: 'Jas',
-//   number: 1234
-// })
-
-// person.save().then(response => {
-//   console.log(`${person.name} saved`)
-//   console.log(person);
-//   mongoose.connection.close()
-// })
-
-Person.find({}).then(response => {
-  response.forEach(person => {
-    console.log(person);
+if (process.argv.length === 3) {
+  Person.find({}).then(response => {
+    response.forEach(person => {
+      console.log(person);
+    })
+    mongoose.connection.close()
   })
-  mongoose.connection.close()
-})
+} else {
+  const person = new Person({
+    name: `${name}`,
+    number: number
+  })
+
+  person.save().then(response => {
+    console.log(`added ${person.name} ${person.number} to phonebook`)
+    mongoose.connection.close()
+  })
+}
